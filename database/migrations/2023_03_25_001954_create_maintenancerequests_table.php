f03ca9ab9dde05e353da56006eeb971b7a7a5f2b
Mariana0416 <sanchezmariana2307@gmail.com>
2023-04-10 19:36:05 -0600

    correción de campos de la BD

--- database/migrations/2023_03_25_001954_create_maintenancerequests_table.php
+++ database/migrations/2023_03_25_001954_create_maintenancerequests_table.php
@@ -16,12 +16,14 @@ return new class extends Migration
         Schema::create('maintenancerequests', function (Blueprint $table) {
             $table->id();
             $table->string('department');
-            $table->integer('folio'); //se eliminó la columna, se usará en su lugar el id
             $table->date('requestDate');
             $table->unsignedBigInteger('personaldata_id');
             $table->foreign('personaldata_id')->references('id')->on('personaldatas')->onDelete('cascade')->onUpdate('cascade');
             $table->string('requestDescription');
             $table->enum('status',['Pendiente','Por liberar','Liberada']);
+            $table->string('evidence1')->nullable();
+            $table->string('evidence2')->nullable();
+            $table->string('evidence3')->nullable();
             $table->timestamps();
         });
     }
